524fa8b94f6d257ae3f11274de11448517195165
SEYDOUD <seydoudansogo00@gmail.com>
2025-08-24 14:18:04 +0000

    Promise chaining section

---
+++ 09-asynchronous-javascript/09-promise-chaining/script.js
@@ -0,0 +1,28 @@
+const promise = new Promise((resolve , reject)=>{
+    // Do some async test
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({name: 'John' , age: 30})
+        }else{
+            reject('Error: Something went wrong')
+        }
+    }, 1000);
+})
+promise
+    .then((user) => {
+        console.log(user)
+        return user.name
+    })
+    .then((name)=>{
+        console.log(name);
+        return name.length
+    })
+    .then((nameLength)=>{
+        console.log(nameLength);
+    })
+    .catch((error)=> {
+        console.log(error);
+        return 123;
+    })
+    .then((x) => console.log("come",x))
