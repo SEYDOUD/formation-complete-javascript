ff800989acddfd150c72ba617fdbc1e816341c83
SEYDOUD <seydoudansogo00@gmail.com>
2025-11-21 11:23:18 +0100

    sound manager

--- 21-ambient-sounds-project/js/app.js
+++ 21-ambient-sounds-project/js/app.js
@@ -1,10 +1,11 @@
 import { sounds , defaultPresets } from "./soundData.js";
+import { SoundManager } from "./soundManager.js";
 
 class AmbientMixer{
     // Initialize dependencies and default state
     constructor(){
         console.log('Initializing state...')
-        this.soundManager = null
+        this.soundManager = new SoundManager()
         this.ui = null
         this.presetManager = null
         this.timer = null
@@ -14,12 +15,24 @@ class AmbientMixer{
 
     init(){
         try {
-            console.log('Initializing app...')
+            // Load all sound files
+            this.loadAllSounds()
+
             this.isInitialized = true
         } catch (error) {
             console.error('Failed to initialized app: ',error)
         }
     }
+
+    // Load all sound files
+    loadAllSounds(){
+        sounds.forEach((sound) => {
+            const audioUrl = `audio/${sound.file}`;
+            const success = this.soundManager.loadSound(sound.id , audioUrl)
+            if(!success) console.warn(`Could not lead sound: ${sound.name} from ${audioUrl}`)
+        })
+    }
+
 }
 
 // Initializing app when the dom is ready
---
+++ 21-ambient-sounds-project/js/soundManager.js
@@ -0,0 +1,24 @@
+export class SoundManager{
+    constructor() {
+        this.audioElements = new Map()
+        this.isPlaying = false
+        console.log('SoundManager created')
+    }
+
+    // Load a sound file
+    loadSound(soundId , filePath){
+        try {
+            const audio = new Audio()
+            audio.src = filePath
+            audio.loop = true
+            audio.preload = 'metadata'
+
+            // Add sound to audio elements map
+            this.audioElements.set(soundId , audio)
+            return true
+        } catch (error) {
+            console.log(`Failed to load sound ${soundId}`)
+            return false
+        }
+    }
+}
